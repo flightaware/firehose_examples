497d79d752ab9851490e7d43884f0b5adc35bba7
Shannon Noe <shannon.noe@flightaware.com>
2018-05-31 11:43:55 -0500

    Add comments and cleanups

--- java/example3/src/main/java/com/flightaware/firehose/examples/Example3.java
+++ java/example3/src/main/java/com/flightaware/firehose/examples/Example3.java
@@ -20,7 +20,7 @@ class FlightObject {
     String orig;
     String dest;
     String aat; // actual arrival time
-    String adt; // actual depature time
+    String adt; // actual departure time
     String reg;
 
     @Override
@@ -51,6 +51,7 @@ class Flight {
         String adt = "";
         String aat = "";
 
+        // Handle all cases where null can be present
         if (departure != null) {
             orig = departure.orig;
             adt = departure.adt;
@@ -65,7 +66,9 @@ class Flight {
             id = arrival.id;
         }
 
-        if (id == null) throw new RuntimeException("id cannot be null " + departure.toString() + " " + arrival.toString());
+        if (id == null) {
+            throw new RuntimeException("id cannot be null " + departure.toString() + " " + arrival.toString());
+        }
         if (ident == null) ident = "";
         if (orig == null) orig = "";
         if (dest == null) dest = "";
@@ -77,12 +80,30 @@ class Flight {
     }
 }
 
+/*
+ * Aggregate all of the departure and arrival messages into a single object.
+ * Join the messages based on the id key.
+ *
+ * Since we join the feed in the past, we will have arrivals without departures.
+ */
 public class Example3 {
 
+    // Our flight board state is saved in a HashMap in RAM
+    // The key is the id field, the flight id FA generates.
     private static final Map<String, Flight> flights = new HashMap();
 
+    // GZIP the data on the network wire
     private static final boolean useCompression = false;
 
+    //
+    // Either pass the username and API access token on the command line or
+    // set properties
+    //
+    // You can run with maven using the target verify to run the program.
+    // Fill in your username and API key
+    //
+    // mvn -Dfirehose.username=##### -Dfirehose.password=###### verify
+    //
     public static void main(String[] args) {
         String machineName = "firehose.flightaware.com";
         if (args.length != 2) {
@@ -93,6 +114,9 @@ public class Example3 {
         System.out.println(" Thank you for using FlightAware ... bye now");
     }
 
+    /*
+     * Print out the hash map as CSV
+     */
     private static void PrintBoard() {
         long now = System.currentTimeMillis();
         now = TimeUnit.SECONDS.convert(now, TimeUnit.MILLISECONDS);
@@ -102,16 +126,18 @@ public class Example3 {
     }
 
     private static void RunClient(String machineName, String username, String password) {
-        // Request 3 days ago
+        // Request data from 3 days ago
+        // It can take awhile to catch up, but this insures we get all the departures
+        // 3 days is a bit conservative
         long startTime = System.currentTimeMillis();
         startTime = TimeUnit.SECONDS.convert(startTime, TimeUnit.MILLISECONDS);
         startTime = startTime - TimeUnit.DAYS.toSeconds(3L);
+
         // Initiate data stream
         String initiation_command = String.format("pitr " + startTime + " username %s password %s", username, password);
         try {
             SSLSocket ssl_socket;
             ssl_socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(machineName, 1501);
-            // enable certifcate validation:
             SSLParameters sslParams = new SSLParameters();
             sslParams.setEndpointIdentificationAlgorithm("HTTPS");
             ssl_socket.setSSLParameters(sslParams);
@@ -138,10 +164,15 @@ public class Example3 {
             String message;
             long start = System.currentTimeMillis();
             while ((message = reader.readLine()) != null) {
+                // Parse the JSON with Googles GSON
+                // Any missing fields will be null
                 FlightObject flight = gson.fromJson(message, FlightObject.class);
+                // Filter by message type
                 if (flight.type.equals("arrival") || flight.type.equals("departure")) {
+                    // Get last value from map
 	                Flight f = flights.get(flight.id);
 	                if (f == null) {
+	                    // Add a value to map
 	                    f = new Flight();
                         flights.put(flight.id, f);
                     }
@@ -151,6 +182,7 @@ public class Example3 {
 	                } else {
 	                    f.departure = flight;
 	                }
+	                // Every 30 seconds print out the collection
 	                long now = System.currentTimeMillis();
 	                if (now - start > TimeUnit.SECONDS.toMillis(30)) {
                         start = now;
